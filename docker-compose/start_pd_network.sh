511d26cba9d0f51e00a08e9352e53f483266690f
Valik Grinko <valik82@gmail.com>
2018-09-30 17:59:55 +0300

    deploy

--- docker-compose/start_pd_network.sh
+++ docker-compose/start_pd_network.sh
@@ -2,7 +2,7 @@ BITSHARESD_WITNESS_ID=\"1.6.1\"
 BITSHARESD_PRIVATE_KEY=[\"PVD6wGqbawxvcK7Bpgiu7QaaAD54CmaB9Hh8mRUVPzq4EthbEvJaN\",\"5Jg7GUoFL1YnUVQ5j7CjDcZPBSo83QgUHia9Et5t7D6n2f3M5Mj\"]
 
 /usr/local/bin/witness_node \
- --genesis-json=/startup/genesis.json \
+ --genesis-json=/startup/pvdgenesis.json \
  --witness-id=$BITSHARESD_WITNESS_ID \
  --private-key=$BITSHARESD_PRIVATE_KEY \
  --enable-stale-production
